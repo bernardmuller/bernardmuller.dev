51240dd4f0a99a7e06d863cffecfc3ae379faaef
Bernard Muller <b.mullerjnr@gmail.com>
2023-09-14 13:50:20 +0200

    Update Bio.tsx

--- components/bio/Bio.tsx
+++ components/bio/Bio.tsx
@@ -17,7 +17,19 @@ const Bio = ({
 	return (
 		<div className="flex justify-center w-full pt-16">
 			<div className="flex gap-6 w-full">
-				<div className="flex flex-col-reverse gap-6 items-center text-center lg:flex-row lg:w-full lg:gap-0 lg:justify-between lg:items-start lg:text-left">
+				<div className="flex flex-col-reverse gap-6 items-center text-center lg:flex-row lg:w-full lg:gap-0 lg:items-start lg:text-left">
+					<div className="grid">
+						{image && (
+							<div className="relative flex justify-center items-center h-64 w-56 p-2 bg-black_l">
+								<Image
+									src={urlFor(image).url()}
+									alt="profile_pic"
+									className="relative w-full h-full object-cover overflow-clip p-2"
+									fill
+								/>
+							</div>
+						)}
+					</div>
 					<div className="grid gap-2 lg:w-[70%] text-grey_l">
 						<span className=" text-4xl text-white">{greeting}</span>
 						<span className=" text-grey_l">
@@ -33,18 +45,7 @@ const Bio = ({
 							components={RichTextComponents as any}
 						/>
 					</div>
-					<div className="grid">
-						{image && (
-							<div className="relative flex justify-center items-center h-64 w-56 p-2 bg-black_l">
-								<Image
-									src={urlFor(image).url()}
-									alt="profile_pic"
-									className="relative w-full h-full object-cover overflow-clip p-2"
-									fill
-								/>
-							</div>
-						)}
-					</div>
+					
 				</div>
 			</div>
 		</div>
